afa70715a1873eaaa2c0d9a05a1b3c75768a35f4
PratikC2003 <pratik@collegekhabri.com>
2026-07-26 12:32:24 +0530

    fix: use object format for document_order in Zoho Sign request

--- src/lib/zoho-sign.ts
+++ src/lib/zoho-sign.ts
@@ -304,7 +304,7 @@ export async function sendAgreement(opts: {
         is_sequential: true,
         reminder_period: 3,
         document_ids: [{ document_id: docId }],
-        document_order: [docId],
+        document_order: [{ document_id: docId }],
       },
     };
 
